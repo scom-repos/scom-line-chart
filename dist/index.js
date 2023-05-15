7e2b91d2fc3956ae74639051ecd6111a2c8443b7
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-05-15 18:57:08 +0800

    Update API endpoint

--- dist/index.js
+++ dist/index.js
@@ -189,7 +189,7 @@ define("@scom/scom-line-chart/data.json.ts", ["require", "exports"], function (r
     ///<amd-module name='@scom/scom-line-chart/data.json.ts'/> 
     exports.default = {
         defaultBuilderData: {
-            apiEndpoint: 'https://api.dune.com/api/v1/query/2360905/results?api_key=324WhvsCHWCji2pkgtfa0JDqDu8j0FdD',
+            apiEndpoint: "/dune/query/2360905",
             options: {
                 title: 'ETH Withdrawals after Shanghai Unlock vs ETH price',
                 options: {
